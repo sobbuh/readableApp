c2f657885fa6ab4043afa1a98403928cd917ba92
sobbuh <sobbuh7@gmail.com>
2017-08-14 15:54:29 -0700

    create initial state

---
+++ src/store/initialState.js
@@ -0,0 +1,14 @@
+export default {
+  categories: {
+    categories:[],
+    fetching: false
+  },
+  posts: {
+    posts: [],
+    fetching: false,
+  },
+  comments: {
+    comments: [],
+    fetching: false
+  }
+};
